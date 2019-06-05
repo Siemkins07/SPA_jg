35b04260c2dbcddd6de5dcbeef1632f81e8cd677
Jacek Pobiarżyn <jacek.pobiarzyn@gmial.com>
2019-06-05 16:39:11 +0200

    add photos, folders, components, navigation.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './layouts/Header';
+import Navigation from './layouts/Navigation';
+import Page from './layouts/Page';
+import Footer from './layouts/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      dziala w opor
-    </div>
-  );
+
+class App extends Component {
+  state = {}
+  render() {
+
+    return (
+      <Router>
+        <div className='app'>
+          <header>
+            {<Header />}
+          </header>
+          <main>
+            <aside>
+              {<Navigation />}
+            </aside>
+            <section className='page'>
+              {<Page />}
+            </section>
+          </main>
+          <footer>
+            {<Footer />}
+          </footer>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/layouts/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navigation.css';
+
+const Navigation = () => {
+
+    const navigationList = [
+        { name: "start", path: "/", exact: true },
+        { name: "książka", path: "/book" },
+        { name: "kontakt", path: "/contact" },
+        { name: "panel admina", path: "/admin" },
+    ];
+
+    const menu = navigationList.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} exact={item.exact ? item.exact : null}>{item.name}</NavLink>
+        </li>
+    ))
+
+
+    return (
+        <nav className="left-nav">
+            <ul>
+                {menu}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
