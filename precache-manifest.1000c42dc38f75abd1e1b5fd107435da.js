db4ca59333541d8d211903d9e28738674e6cc92e
Jacek Pobiarżyn <jacek.pobiarzyn@gmial.com>
2019-06-12 15:44:39 +0200

    Updates

---
+++ precache-manifest.1000c42dc38f75abd1e1b5fd107435da.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3cf2dc181418c384e052a2370f37c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "514d27af298bed161a1b",
+    "url": "/static/css/main.2100cc22.chunk.css"
+  },
+  {
+    "revision": "d8324352946c9e831e24",
+    "url": "/static/js/2.ca507041.chunk.js"
+  },
+  {
+    "revision": "514d27af298bed161a1b",
+    "url": "/static/js/main.d7f96183.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01d5a447731b1d40ade117fb8daa09bf",
+    "url": "/static/media/foto1.01d5a447.jpg"
+  },
+  {
+    "revision": "18f02d08073e8d5436880c06bc0ddb5c",
+    "url": "/static/media/foto2.18f02d08.jpg"
+  },
+  {
+    "revision": "acc7230ad41525c257b73324a27312a1",
+    "url": "/static/media/foto3.acc7230a.jpg"
+  },
+  {
+    "revision": "f80ce76e00d89a3bef51582cd62d9d93",
+    "url": "/static/media/foto4.f80ce76e.jpg"
+  },
+  {
+    "revision": "b1bd3e67807b919a5dc391140f687b88",
+    "url": "/static/media/foto5.b1bd3e67.jpg"
+  },
+  {
+    "revision": "258254e8e4d8a8ffd5d607bf0af79ec7",
+    "url": "/static/media/foto6.258254e8.jpg"
+  },
+  {
+    "revision": "e10193240c31be5ef404d1cf151d88d1",
+    "url": "/static/media/foto7.e1019324.jpg"
+  },
+  {
+    "revision": "cae52b2181b5eff6b7bdd20482dea193",
+    "url": "/static/media/foto8.cae52b21.jpg"
+  },
+  {
+    "revision": "8c8bc541689bd50a8335b1c742ebb2df",
+    "url": "/static/media/foto9.8c8bc541.jpg"
+  },
+  {
+    "revision": "4428888a404bbcc373e60e5545a2ff87",
+    "url": "/static/media/start.4428888a.jpg"
+  }
+]);
